803923409af5108fcd696d99a9e6f826486feaf6
Sajid Zeya <sajidzeya294@gmail.com>
2025-02-17 12:24:01 +0530

    Read Write File

---
+++ Day1/p5.js
@@ -0,0 +1,9 @@
+const fs=require('fs');
+const write=()=>{
+    const data="I am updated data"
+    fs.writeFile('./data.txt',data,(err)=>{
+        if(err) console.log(err);
+        else console.log("File updated successfully");
+    })
+}
+write();
